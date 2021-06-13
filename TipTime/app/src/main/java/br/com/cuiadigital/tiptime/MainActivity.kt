393fefee406c930a1716ba327be2459ad711eca9
Julio Gomes <juliorgm@gmail.com>
2021-06-13 17:10:55 -0300

    tip_time calculate method

--- TipTime/app/src/main/java/br/com/cuiadigital/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/br/com/cuiadigital/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,19 +18,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when(selectedId){
+        val stringInTextField : String = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0){
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent ->0.2
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+
         var tip = cost * tipPercentage
-        val tipRounded = binding.roundUpSwitch.isChecked
-        if (tipRounded){
+        if (binding.roundUpSwitch.isChecked){
             tip = kotlin.math.ceil(tip)
         }
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.displayTip.text = getString(R.string.tip_amount, formattedTip)
     }
